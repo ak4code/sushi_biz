ca43e03d92458572da53616d6c2c53c390618462
akweb <haritonov.aka@gmail.com>
2020-10-06 23:26:01 +0300

    fix: dir

--- sushi_biz/prod.py
+++ sushi_biz/prod.py
@@ -11,8 +11,12 @@ STATIC_ROOT = BASE_DIR / 'static'
 
 ALLOWED_HOSTS = [x for x in env.list('ALLOWED_HOSTS')]
 
+STATICFILES_DIRS = (
+    BASE_DIR / 'dist',
+)
+
 if not DEBUG:
     WEBPACK_LOADER.update({
-        'BUNDLE_DIR_NAME': 'dist/',
+        'BUNDLE_DIR_NAME': '/',
         'STATS_FILE': BASE_DIR / 'webpack-stats-prod.json'
     })
